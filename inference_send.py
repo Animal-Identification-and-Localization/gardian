58ced85ab954e4809a04137d77a8013387ed3d97
Shane Raible <ssraible@gmail.com>
2022-10-09 23:14:48 -0400

    merge inference and comms v1

---
+++ inference_send.py
@@ -0,0 +1,17 @@
+from boardcomms.coral_coms.coms_py.coral_pb_out import send_dx_dy
+
+from inference.detector import Detector
+
+def main():
+    model_path = "../model/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite"
+    labels_path = "../model/coco_labels.txt"
+    input_image_path = "../images/cats2.jpg"
+    output_image_path = "../images/Doutput.jpg"
+
+    d1 = Detector(model_path, labels_path, .4)
+    object_list = d1.run_detection(input_image_path, output_image_path)
+    print(object_list)
+    
+
+if __name__ == "__main__":
+    main()
